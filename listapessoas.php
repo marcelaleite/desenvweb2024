8b638141d2a7ff559841539875210eba08c56331
Marcela Leite <marcela.leite@gmail.com>
2024-03-05 16:53:10 -0300

    atualização + update

--- listapessoas.php
+++ listapessoas.php
@@ -1,3 +1,31 @@
+<?php
+
+define('USUARIO', 'marr'); /// root
+define('SENHA', 'marR.123#'); // ''
+define('HOST', 'localhost'); 
+define('PORT', '3306'); 
+define('DB', 'contatos'); 
+define('DSN', "mysql:host=".HOST.";port=".PORT.";dbname=".DB.";charset=UTF8");
+
+// conectar com o banco 
+$conexao = new PDO(DSN, USUARIO, SENHA);
+
+// montar consulta
+$sql = "SELECT * FROM pessoa WHERE id = :id";
+$id =  isset($_GET['id'])?$_GET['id']:0; // pegar busca
+if ($id > 0){
+    // prepara o comando
+    $comando = $conexao->prepare($sql); // preparar comando
+    // vincular os parâmetros
+    $comando->bindValue(':id',$id);
+    // executar consulta
+    $comando->execute(); // executar comando
+    // listar o resultado da consulta
+    $contato = $comando->fetch();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,11 +40,11 @@
         <fieldset>
             <legend>Cadastro de Contatos</legend>        
                 <label for="id">Id:</label>
-                <input type="text" name="id" id="id" value="" readonly>
+                <input type="text" name="id" id="id" value="<?php if(isset($contato)) echo $contato['id']?>" readonly>
                 <label for="nome">Nome:</label>
-                <input type="text" name="nome" id="nome" value="">
+                <input type="text" name="nome" id="nome" value="<?php if(isset($contato)) echo $contato['nome']?>">
                 <label for="telefone">Telefone:</label>
-                <input type="text" name="telefone" id="telefone" value="">
+                <input type="text" name="telefone" id="telefone" value="<?php if(isset($contato)) echo $contato['telefone']?>">
                 <button type='submit'>Salvar</button>
                 <button type='reset'>Cancelar</button>
         </fieldset>
@@ -48,48 +76,9 @@
             <th>Telefone</th>
         </tr>
         <?php
-
-            define('USUARIO', 'marr'); /// root
-            define('SENHA', 'marR.123#'); // ''
-            define('HOST', 'localhost'); 
-            define('PORT', '3306'); 
-            define('DB', 'contatos'); 
-            define('DSN', "mysql:host=".HOST.";port=".PORT.";dbname=".DB.";charset=UTF8");
-            
-            // conectar com o banco 
-            $conexao = new PDO(DSN, USUARIO, SENHA);
-  
-            // montar consulta
-            $sql = "SELECT * FROM pessoa";
-
-            $busca =  isset($_GET['busca'])?$_GET['busca']:0; // pegar busca
-            $tipo =  isset($_GET['tipo'])?$_GET['tipo']:0; // pegar busca
-
-            if ($tipo > 0 )
-                switch($tipo){
-                    case 1: $sql .= " WHERE id = :busca"; break;
-                    case 2: $sql .= " WHERE nome like :busca"; $busca = "%{$busca}%"; break;
-                    case 3: $sql .= " WHERE telefone like :busca";  $busca = "%{$busca}%";  break;
-
-                }
-    
-            // prepara o comando
-            $comando = $conexao->prepare($sql); // preparar comando
-            // vincular os parâmetros
-            if ($tipo > 0 )
-                $comando->bindValue(':busca',$busca);
-            
-            // executar consulta
-            $comando->execute(); // executar comando
-
-            // listar o resultado da consulta
-            $lista = $comando->fetchAll();
-  
-            foreach($lista as $pessoa){
-                echo "<tr><td>".$pessoa['id']."</td><td>".$pessoa['nome']."</td><td>".$pessoa['telefone']."</td></tr>";
            
-           
-            }
+  
+            // Inserir e alterar dados
             if ($_SERVER['REQUEST_METHOD'] == 'POST'){
                 $id =  isset($_POST['id'])?$_POST['id']:0; 
                 $nome =  isset($_POST['nome'])?$_POST['nome']:0; 
@@ -112,9 +101,39 @@
                     $comando->bindValue(':telefone',$telefone);
                 }
                 if ($comando->execute())
-                    echo "Dados inseridos com sucesso!";
+                    echo "Dados inseridos/alterados com sucesso!";
                 else
                     echo "erro ao inserir dados!";
+            }
+            //  Listagem e Pesquisa
+            // montar consulta
+            $sql = "SELECT * FROM pessoa";
+
+            $busca =  isset($_GET['busca'])?$_GET['busca']:0; // pegar busca
+            $tipo =  isset($_GET['tipo'])?$_GET['tipo']:0; // pegar busca
+
+            if ($tipo > 0 )
+                switch($tipo){
+                    case 1: $sql .= " WHERE id = :busca"; break;
+                    case 2: $sql .= " WHERE nome like :busca"; $busca = "%{$busca}%"; break;
+                    case 3: $sql .= " WHERE telefone like :busca";  $busca = "%{$busca}%";  break;
+
+                }
+
+            // prepara o comando
+            $comando = $conexao->prepare($sql); // preparar comando
+            // vincular os parâmetros
+            if ($tipo > 0 )
+                $comando->bindValue(':busca',$busca);
+
+            // executar consulta
+            $comando->execute(); // executar comando
+
+            // listar o resultado da consulta
+            $lista = $comando->fetchAll();
+
+            foreach($lista as $pessoa){
+                echo "<tr><td><a href='listapessoas.php?id=".$pessoa['id']."'>".$pessoa['id']."</a></td><td>".$pessoa['nome']."</td><td>".$pessoa['telefone']."</td></tr>";
             }
         ?>
     </table>
