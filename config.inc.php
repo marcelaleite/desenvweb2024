ab84847f44fed293d0c913f14b5ec2e143bd8948
Marcela Leite <marcela.leite@gmail.com>
2024-04-01 09:05:01 -0300

    atualização

--- config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('USUARIO', 'marr'); /// root
-define('SENHA', 'marR.123#'); // ''
-define('HOST', 'localhost'); 
-define('PORT', '3306'); 
-define('DB', 'contatos'); 
-define('DSN', "mysql:host=".HOST.";port=".PORT.";dbname=".DB.";charset=UTF8");
-
