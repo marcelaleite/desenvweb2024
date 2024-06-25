47cd74dac91a1d3c541fb3f6c5a27210ec27b2e3
Marcela Leite <marcela.leite@gmail.com>
2024-06-25 17:07:27 -0300

    alteração

--- login/login.php
+++ login/login.php
@@ -10,6 +10,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     try{
         // criar o objeto Pessoa que irá persistir os dados 
         $pessoa = Login::efetuarLogin($usuario,$senha);
+        if ($pessoa){
+            session_start();
+            $_SESSION['idusuario'] = $pessoa->getId();
+            $_SESSION['nome'] = $pessoa->getNome();
+            header('location: ../pessoa/index.php');
+
+        }
+        else{
+            echo "Efetuar login";
+        }
     }catch(Exception $e){ // caso ocorra algum erro na validação das regras de negócio dispara uma exceção
         header('location: index.php?MSG=Erro: '.$e->getMessage()); // direciona para o incio com a mensagem de erro
     }
+}
